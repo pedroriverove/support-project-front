a1f58f6d278aaec3be98c4a3111640eec42a17ff
Pedro Rivero <pedro.rivero.32119@gmail.com>
2022-05-17 05:15:59 -0400

    Fuente inter, iconos fortawesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import '@/assets/css/app.css';
 import '@/assets/css/index.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
