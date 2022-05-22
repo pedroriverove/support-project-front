2be04684f0cfe7bcdd3a0a308275cea2f66755e2
Pedro Rivero <pedro.rivero.32119@gmail.com>
2022-05-22 19:25:37 -0400

    Conectando endpoint de usuario filtrando por roles

--- src/services/ticket.service.ts
+++ src/services/ticket.service.ts
@@ -2,32 +2,32 @@ import http from '@/http-common';
 import TicketInterface from '@/interfaces/ticket.interface';
 import TicketFactory from '@/factories/ticket.factory';
 
-const getAll = () => {
-    return http.get<Array<TicketInterface>>("/v1/tickets");
+const create = (data: TicketFactory) => {
+    return http.post<TicketFactory>("/v1/tickets", data);
 };
 
 const get = (id: number) => {
     return http.get<TicketInterface>(`/v1/tickets/${id}`);
 };
 
-const create = (data: TicketFactory) => {
-    return http.post<TicketFactory>("/v1/tickets", data);
-};
-
-const update = (id: number, data: TicketFactory) => {
-    return http.put<any>(`/v1/tickets/${id}`, data);
+const getAll = () => {
+    return http.get<Array<TicketInterface>>("/v1/tickets");
 };
 
 const remove = (id: number) => {
     return http.delete<any>(`/v1/tickets/${id}`);
 };
 
+const update = (id: number, data: TicketFactory) => {
+    return http.put<any>(`/v1/tickets/${id}`, data);
+};
+
 const TicketService = {
-    getAll,
-    get,
     create,
-    update,
-    remove
+    get,
+    getAll,
+    remove,
+    update
 };
 
 export default TicketService;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,18 @@
 import http from '@/http-common';
-import UserInterface from "@/interfaces/user.interface";
+import UserInterface from '@/interfaces/user.interface';
+import TicketInterface from '@/interfaces/ticket.interface';
 
 const getAll = () => {
     return http.get<Array<UserInterface>>("/v1/users");
 };
 
+const getRole = (name: string) => {
+    return http.get<TicketInterface>(`/v1/users/${name}`);
+};
+
 const UserService = {
-    getAll
+    getAll,
+    getRole
 };
 
 export default UserService;
